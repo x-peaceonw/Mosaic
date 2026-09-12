97371b565ce7cdd181e39d24f5568363eff2011b
x-peaceonw <109201461+x-peaceonw@users.noreply.github.com>
2026-09-12 16:35:42 -0500

    Reorder and update artist lists

--- update_feed.py
+++ update_feed.py
@@ -14,8 +14,8 @@ SPOTIFY_CLIENT_ID = os.getenv("CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 TICKETMASTER_API_KEY = os.getenv("TICKETMASTER_API_KEY")
 
-ARTISTS = ["SZA", "Beyonce","Jhene Aiko", "Olivia Dean", "Coldplay", "Qendresa", "Solange", "Tori Kelly", "Rascal Flatts", "Sondae","Ariana Grande"]
-TICKET_ARTISTS = ["Beyonce", "Coldplay","Jhene Aiko", "Tori Kelly"]
+ARTISTS = ["Olivia Dean","SZA", "Beyonce","Jhene Aiko", "Olivia Dean", "Coldplay", "Qendresa", "Solange", "Tori Kelly", "Rascal Flatts", "Sondae","Ariana Grande"]
+TICKET_ARTISTS = ["Beyonce", "Coldplay","Jhene Aiko", "Tori Kelly","Olivia Dean"]
 NEWS_TOPICS = [
     {"query": "Zendaya fashion OR style OR outfit", "category": "Zendaya", "artist": "Zendaya"},
     {"query": "Spider-Man movie", "category": "Movies", "artist": "Spider-Man"},
